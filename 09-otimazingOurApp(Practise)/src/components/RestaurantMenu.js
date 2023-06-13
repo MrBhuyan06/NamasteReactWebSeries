0b8c5061d805b22a14e3d70320939066585195c6
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-06-13 22:06:33 +0530

    Added resHook

---
+++ 09-otimazingOurApp(Practise)/src/components/RestaurantMenu.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { CND_URL } from "../utils/constants.js";
+import { useParams } from "react-router-dom";
+import Shimmer from "./Shimmer.js";
+import useRestaurent from "../utils/useRestaurent.js";
+const ResTaurantMenu = () => {
+  const { resId } = useParams();
+  console.log(resId);
+
+  const resInfo = useRestaurent(resId);
+
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+  const {
+    name,
+    cuisines,
+    cloudinaryImageId,
+    costForTwoMessage,
+    avgRatingString,
+  } = resInfo?.cards[0]?.card?.card?.info;
+  const { itemCards } =
+    resInfo.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <img src={`${CND_URL}${cloudinaryImageId}`} alt="" />
+      <h3>
+        {cuisines.join(",")}-{costForTwoMessage}
+      </h3>
+
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item, i) => {
+          return (
+            <li key={item.card.info.id}>{`${item.card.info.name} ${
+              item.card.info.price / 100 || item.card.info.defaultPrice / 100
+            }`}</li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default ResTaurantMenu;
