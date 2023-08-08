7988806f46d1755d66fc60539ae2481e82cab24d
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-09 00:42:51 +0530

    Added the  navigation

--- Extra Practise/12-Machine Coding Round (YouTube)/namaste-youtube/src/App.js
+++ Extra Practise/12-Machine Coding Round (YouTube)/namaste-youtube/src/App.js
@@ -4,13 +4,33 @@ import Head from "./componets/Head.js";
 import Body from "./componets/Body.js";
 import { Provider } from "react-redux";
 import store from "./utils/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import VideoContainer from "./componets/VideoContainer.js";
+import WatchPage from "./componets/WatchPage.js";
+import MainContainer from "./componets/MainContainer.js";
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
--- Extra Practise/12-Machine Coding Round (YouTube)/namaste-youtube/src/componets/Body.js
+++ Extra Practise/12-Machine Coding Round (YouTube)/namaste-youtube/src/componets/Body.js
@@ -4,12 +4,13 @@ import MainContainer from "./MainContainer.js";
 import { useSelector } from "react-redux";
 import store from "../utils/store.js";
 import { YOUTUBE_VIDEO_API } from "../utils/contants.js";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex">
       <SideBar />
-      <MainContainer />
+      <Outlet />
     </div>
   );
 };
