fa2b48763001ddedab429a169b5164d5622df8a3
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-18 23:24:09 +0530

    added the watch page

--- Extra Practise/12-Machine Coding Round (YouTube)/namaste-youtube/src/componets/WatchPage.js
+++ Extra Practise/12-Machine Coding Round (YouTube)/namaste-youtube/src/componets/WatchPage.js
@@ -2,16 +2,17 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice.js";
 import { useSearchParams } from "react-router-dom";
+import CommentContainer from "./CommentContainer.js";
 
 const WatchPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  //   console.log(searchParams.get("v"));
+  // console.log(searchParams.get("v"));
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="px-5">
+    <div className="px-5 flex flex-col ml-10">
       <iframe
         width="1000"
         height="500"
@@ -21,6 +22,9 @@ const WatchPage = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+      <div className="mt-5 min-h-[120px]">
+        <CommentContainer />
+      </div>
     </div>
   );
 };
